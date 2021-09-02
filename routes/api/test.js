039e5edcdbba000b0e23373fb9618880283e3c76
Jakku Night <J97k107u45k117n@gmail.com>
2021-09-02 21:12:00 +0000

    Backend refactor. ABM created.

--- routes/api/test.js
+++ routes/api/test.js
@@ -1,124 +1,69 @@
 const express = require('express');
-const db = require('../../db');
+const abm = require('../../lib/amb');
 const router = express.Router();
 
-var ERROR = {
-    status: 'ERROR'
-};
-var OK = {
-    status: 'OK'
-};
-ERROR = JSON.stringify(ERROR);
-OK = JSON.stringify(OK);
 const link = '/api/test/';
 
 // GET ALL:
 router.get(link, async (req, res) => {
-    try {
-        const rows = await db.query('SELECT * FROM test');
-        if(rows.length === 0){
-            console.error("[ERROR]: DATA NOT FOUND!!!");
-            res.json(ERROR);
-        }else{
-            console.log("[SUCCESS]: QUERY OK");
-            res.json(JSON.stringify(rows));
-        }
-    } catch (error) {
-        console.error(error);
-        res.json(ERROR);
-    }
+   res.json(await abm.get_all('test'));
 });
 
 // ADD:
 router.post(link, async (req, res) => {
+    var table;
+    var new_data;
     if(req.body === undefined){
         console.error("[ERROR]; DATA IS IN BLANK!!!");
-        res.json(ERROR);
+        table = '';
+        new_data = {};
     }else{
-        if(req.body.task === '' || req.body.description === ''){
-            console.error("[ERROR]; DATA IS IN BLANK!!!");
-            res.json(ERROR);
-        }else{
-            const { task, description } = req.body;
-            const new_data = {
-                task,
-                description
-            };
-            try {
-                const rows = await db.query('INSERT INTO test SET ?', [new_data]);
-                if(rows.insertedId !== null){
-                    console.log("[SUCCESS]: QUERY OK");
-                    res.json(OK);
-                }else{
-                    console.error("[ERROR]: DATA NOT INSERTED!!!");
-                    res.json(ERROR);
-                }
-            } catch (error) {
-                console.error(error);
-                res.json(ERROR);
-            }
-        }
+        table = 'test';
+        const { task, description } = req.body;
+        new_data = {
+            task,
+            description
+        };
     }
+    res.json(await abm.add_row(table, new_data));
 });
 
 // UPDATE:
 router.put(link, async (req, res) => {
+    var table;
+    var row_data;
+    var id_;
     if(req.body === undefined){
         console.error("[ERROR]; DATA IS IN BLANK!!!");
-        res.json(ERROR);
+        table = '';
+        row_data = {};
+        id_ = '';
     }else{
-        if(req.body.task === '' || req.body.description === '' || req.body.id === ''){
-            console.error("[ERROR]; DATA IS IN BLANK!!!");
-            res.json(ERROR);
-        }else{
-            const { task, description, id } = req.body;
-            const new_data = {
-                task,
-                description
-            };
-            try {
-                const rows = await db.query('UPDATE test SET ? WHERE id = ?', [new_data, id]);
-                if(rows.affectedRows !== 0){
-                    console.log("[SUCCESS]: QUERY OK");
-                    res.json(OK);
-                }else{
-                    console.error("[ERROR]: DATA NOT UPDATED!!!");
-                    res.json(ERROR);
-                }
-            } catch (error) {
-                console.error(error);
-                res.json(ERROR);
-            }
-        }
+        table = 'test';
+        const { task, description, id } = req.body;
+        row_data = {
+            task,
+            description
+        };
+        id_ = id;
     }
+    res.json(await abm.update_row(table, row_data, id));
 });
 
 // DELETE:
 router.delete(link, async (req, res) => {
+    var table;
+    var id_;
     if(req.body === undefined){
         console.error("[ERROR]; DATA IS IN BLANK!!!");
-        res.json(ERROR);
+        id_ = '';
+        table = '';
     }else{
-        if(req.body.id === ''){
-            console.error("[ERROR]; DATA IS IN BLANK!!!");
-            res.json(ERROR);
-        }else{
-            const { id } = req.body;
-            try {
-                const rows = await db.query('DELETE FROM test WHERE id = ?', [id]);
-                if(rows.affectedRows !== 0){
-                    console.log("[SUCCESS]: QUERY OK");
-                    res.json(OK);
-                }else{
-                    console.error("[ERROR]: DATA NOT DELETED!!!");
-                    res.json(ERROR);
-                }
-            } catch (error) {
-                console.error(error);
-                res.json(ERROR);
-            }
-        }
+        const { id } = req.body;
+        id_ = id;
+        table = 'test';
     }
+    res.json(await abm.delete_row(table, id_));
 });
 
 module.exports = router;
